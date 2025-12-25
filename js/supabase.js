857b3a30b2490719860900a7104b9e556553fe0b
Dheeraj Singh <dhrax21bc@gmail.com>
2025-12-25 13:11:30 +0530

    fixed-Supabase-auth

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://vzetfjzfvfhfvcqpkbbd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6ZXRmanpmdmZoZnZjcXBrYmJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDI3OTEsImV4cCI6MjA4MTYxODc5MX0.lJe-FZ1pP1ZM7rXu9Fu2Y6-P01kp2F8hfnoKZ3vUJtQ"
-);
